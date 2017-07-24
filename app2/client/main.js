f412e6e2cb208e8a8dd134fb232d4ac6493c3be6
Michael Jiang <michaelxhj@outlook.com>
2017-07-24 15:27:03 -0400

    added twitter and facebook npm modules. twitter and facebook posting

--- app2/client/main.js
+++ app2/client/main.js
@@ -1,7 +1,19 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import './facebook.js'
 
 import './main.html';
+
+if(Meteor.isClient) {
+  window.fbAsyncInit = function() {
+    setupFacebook();
+
+    //Setup Twitter API
+    Meteor.call('setupTwitterAPI', function(response) {
+      console.log(response);
+    });
+  };
+}
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
@@ -18,5 +30,14 @@ Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
+    /**Twitter Post
+    Meteor.call('postTwitter', "hello w0rld", function(response) {
+      console.log(response);
+    })
+    **/
+    
+    /**Facebook post
+    postFacebook("hello W0rld");
+    **/
   },
 });
