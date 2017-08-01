4afac2957d06514bc93fc939277510595db65553
Yonglin Wang <wangyonglin1999@gmail.com>
2017-08-01 00:43:41 -0400

    Login Page almost done

--- production/imports/ui/pages/login.js
+++ production/imports/ui/pages/login.js
@@ -3,3 +3,11 @@
  */
 import './login.html'
 
+Template.login.events({
+    'click .operation': function (evt, template) {
+        $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    },
+    'submit form': function (evt, template) {
+        FlowRouter.go('stream');
+    }
+})
