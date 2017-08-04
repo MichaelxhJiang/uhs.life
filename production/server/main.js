b1697ba68c61e7b574eca011cb71218d15a33ed2
Michael Jiang <michaelxhj@outlook.com>
2017-08-03 21:42:26 -0400

    two lines of code for google accounts

--- production/server/main.js
+++ production/server/main.js
@@ -2,3 +2,8 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
 });
+
+Accounts.onCreateUser(function (options, user) {
+   //console.log(JSON.stringify(user, null, 2));
+   return user;
+});
