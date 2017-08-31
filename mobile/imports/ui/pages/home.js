1043f92f9e8ae829a2d1b7591c931c12d4996628
Wuon <wuonlabs@gmail.com>
2017-08-31 17:47:44 -0400

    announcement types

--- mobile/imports/ui/pages/home.js
+++ mobile/imports/ui/pages/home.js
@@ -6,4 +6,35 @@ Template.home.events({
     'click .logoutButton': function (evt, template) {
         FlowRouter.go("/");
     },
-})
+});
+
+Template.home.onRendered(function () {
+    // Use the Packery jQuery plugin
+    searchAll();
+});
+
+let counter = 0;
+Template.home.helpers({
+    content: function () {
+        return Session.get('results');
+    },
+    getType: function (i){
+        console.log(Session.get('results').hits[counter].type);
+        return Session.get('results').hits[counter].type;
+    },
+    counter: function(){
+        counter++;
+    }
+});
+
+Template.registerHelper('typeCheck', function(v1, v2) {
+    if (typeof v1 === 'object' && typeof v2 === 'object') {
+        return _.isEqual(v1, v2); // do a object comparison
+    } else {
+        return v1 === v2;
+    }
+});
+
+Template.home.onDestroyed( function(){
+   counter = 0;
+});
