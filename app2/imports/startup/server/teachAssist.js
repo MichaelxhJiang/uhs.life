9dc28fa9e2c7c77638a3922552a71a0f1262972e
Michael Jiang <MichaelxhJiang@users.noreply.github.com>
2017-09-24 15:29:02 -0400

    teach assist reparse course details

--- app2/imports/startup/server/teachAssist.js
+++ app2/imports/startup/server/teachAssist.js
@@ -1,64 +1,73 @@
 Meteor.methods({
     'getTeachAssistTokens' : function(json) {
         if (!(json.student_number && json.password)) {
-            throw new Meteor.error(500, "Need a student number and password");
+            throw new Meteor.Error(400, "Need a student number and password");
         }
-        try {
-            let response = HTTP.call("GET", "https://ta.yrdsb.ca/v4/students/json.php", {
-                data: {
-                    "student_number": json.student_number,
-                    "password": json.password
-                }
-            });
-            let res = JSON.parse(response.content);
-            let id = res[0].student_id;
-            let token = res[0].token;
+        let response = HTTP.call("GET", "https://ta.yrdsb.ca/v4/students/json.php", {
+            data: {
+                "student_number": json.student_number,
+                "password": json.password
+            }
+        });
+        let res = JSON.parse(response.content);
+        if (res[0].ERROR) {
+            console.log(res[0].ERROR);
+            throw new Meteor.Error(403, "Your password is invalid.");
+        }
 
-            console.log(token + " " + id);
-            let tokens = {"student_id":id, "token":token};
-            return tokens;
-        } catch (e) {
-            console.log(e);
-            return -1;
-        }
+        let id = res[0].student_id;
+        let token = res[0].token;
+
+        return {"student_id": id, "token": token};
     },
     'getTeachAssistCourses' : function(json) {
         if (!(json.student_id && json.token)) {
-            throw new Meteor.error(500, "Need a student id and token");
+            throw new Meteor.Error(400, "Need a student id and token");
         }
         console.log("hello courses");
-        try {
-            let response = HTTP.call("GET", "https://ta.yrdsb.ca/v4/students/json.php", {
-                data: {
-                    "student_id": json.student_id,
-                    "token": json.token
-                }
-            });
+        let response = HTTP.call("GET", "https://ta.yrdsb.ca/v4/students/json.php", {
+            data: {
+                "student_id": json.student_id,
+                "token": json.token
+            }
+        });
 
-            return JSON.parse(response.content)[0].data[0].subjects;
-
-        } catch (e) {
-            console.log(e);
-            return -1;
+        if (JSON.parse(response.content)[0].ERROR) {
+            throw new Meteor.Error(400, JSON.parse(response.content)[0].ERROR);
         }
+        return JSON.parse(response.content)[0].data[0].subjects;
+
     },
     'getTeachAssistCourseDetails' : function(json) {
         if (!(json.student_id && json.token && json.subject_id)) {
-            throw new Meteor.error(500, "Need a student id and token and subject id");
+            throw new Meteor.Error(400, "Need a student id and token and subject id");
         }
-        console.log("hello courses details");
-        try {
-            let response = HTTP.call("GET", "https://ta.yrdsb.ca/v4/students/json.php", {
-                data: {
-                    "student_id": json.student_id,
-                    "token": json.token,
-                    "subject_id": json.subject_id
-                }
-            });
-            return JSON.parse(response.content);
-        } catch(e) {
-            console.log(e);
-            return -1;
+        let response = HTTP.call("GET", "https://ta.yrdsb.ca/v4/students/json.php", {
+            data: {
+                "student_id": json.student_id,
+                "token": json.token,
+                "subject_id": json.subject_id
+            }
+        });
+        if (JSON.parse(response.content)[0].ERROR) {
+            throw new Meteor.Error(400, JSON.parse(response.content)[0].ERROR);
         }
+        //console.log(JSON.parse(response.content)[0]);
+
+        let preParse = JSON.parse(response.content)[0];
+        let postParse = JSON.parse(response.content)[0];
+
+        postParse.data.assessment = [];
+
+        let cnt = 0;
+        _.each(preParse.data.assessment.data, function(k, v) {
+            if (v === 'categories') {
+                 postParse.categories = k;
+            } else {
+                postParse.data.assessment[cnt++] = k;
+            }
+        })
+
+        return postParse;
     }
-})
+});
