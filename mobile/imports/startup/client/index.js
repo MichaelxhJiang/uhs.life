0ff843fb26b1da9aaa5df640f27d1ed441de84c8
Michael Jiang <michaelxhj@outlook.com>
2017-10-29 18:01:16 -0400

    merge

--- mobile/imports/startup/client/index.js
+++
@@ -1,8 +0,0 @@
-import './routes.js'
-import './imports.js'
-
-Session.setDefault("DocumentTitle","uhs.life - By Students. For Students.");
-
-Deps.autorun(function(){
-    document.title = Session.get("DocumentTitle");
-});
--- mobile/imports/ui/components/sidebar.js
+++
@@ -1,5 +0,0 @@
-import {Template} from 'meteor/templating';
-import {Session} from 'meteor/session';
-import {FlowRouter} from 'meteor/kadira:flow-router';
-
-import './sidebar.html'
