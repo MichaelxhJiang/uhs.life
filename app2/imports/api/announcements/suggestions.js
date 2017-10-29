c23d259efff83b9236fcff3a072ed6993e39b22a
Yonglin Wang <wangyonglin1999@gmail.com>
2017-10-29 12:32:54 -0400

    rootify

--- app2/imports/api/announcements/suggestions.js
+++
@@ -1,3 +0,0 @@
-import { Mongo } from 'meteor/mongo';
-
-export const Suggestions = new Mongo.Collection('suggestions');
--- mobile/imports/ui/layouts/applicationLayout.js
+++
@@ -1,7 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { ReactiveVar } from 'meteor/reactive-var';
-import { ReactiveDict } from 'meteor/reactive-dict';
-import { Template } from 'meteor/templating';
-import { FlowRouter } from 'meteor/kadira:flow-router';
-
-import './applicationLayout.html';
