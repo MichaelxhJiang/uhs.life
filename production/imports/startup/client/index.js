22b903d0048619b2920d6eb71308f6edfde1d246
Yonglin Wang <wangyonglin1999@gmail.com>
2017-10-29 11:54:09 -0400

    small fixes

--- production/imports/startup/client/index.js
+++ production/imports/startup/client/index.js
@@ -2,12 +2,20 @@
  * This file defines the entry point of the client application, all initialization
  * behaviors are defined here.*/
 'use strict';
+import { TAPi18n } from 'meteor/tap:i18n'
 import './imports.js';
 import './routes.js';
 
 Deps.autorun(function(){
     Session.setDefault('searchContent', {hits:[]});
     document.title = Session.get("DocumentTitle");
+    TAPi18n.setLanguage('en')
+        .done(function () {
+        })
+        .fail(function (error_message) {
+            // Handle the situation
+            console.log(error_message);
+        });
 });
 
 function getUserLanguage() {
@@ -16,12 +24,6 @@ function getUserLanguage() {
 
 if (Meteor.isClient) {
     Meteor.startup(function () {
-        TAPi18n.setLanguage('en')
-            .done(function () {
-            })
-            .fail(function (error_message) {
-                // Handle the situation
-                console.log(error_message);
-            });
+
     });
 }
