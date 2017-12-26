2caa735583b1cafe95f47872d1bccb08ca6f5ec4
Yonglin Wang <wangyonglin1999@gmail.com>
2017-12-25 22:35:22 -0500

    access calendar via stream

--- imports/ui/pages/calendar.js
+++ imports/ui/pages/calendar.js
@@ -1,18 +1,43 @@
-import {moment} from 'meteor/momentjs:moment';
-
 import './calendar.html';
 Template.calendar.onRendered(function(){
-    setTitle('Calendar');
-    this.$('.calendar').fullCalendar({
-        header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,listYear'
-          },
-        googleCalendarApiKey:'AIzaSyCYvWxCfU4knsjla015YTHeGHChKdrDf_c',
-        events: 'uhslifeoffficial@gmail.com',
-        loading: function(state){
-            $('.loader').toggle(state);
-        }
-    });
+    
+});
+
+Template.fullpagecal.onRendered(function(){
+    $('#closeCal').hide();
+    
+});
+
+Template.fullpagecal.events({
+    'click #openCal': function(){
+        $('#openCal').fadeOut('fast', function(){
+            $('#closeCal').fadeIn('fast');
+        });
+        $('.nav-comp').fadeOut('fast', function(){
+            $('.calendar-body').fadeIn(500, function(){
+                $('.calendar').fullCalendar({
+                    header: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'month,listYear'
+                    },
+                    googleCalendarApiKey:'AIzaSyCYvWxCfU4knsjla015YTHeGHChKdrDf_c',
+                    events: 'uhslifeoffficial@gmail.com',
+                    loading: function(state){
+                        $('.loader').toggle(state);
+                    }
+                });    
+            });
+            $('.main').hide();
+        });
+    },
+    'click #closeCal': function(){
+        $('.main').show();
+        $('#closeCal').fadeOut('fast', function(){
+            $('#openCal').fadeIn('fast');
+        });
+        $('.calendar-body').fadeOut(500, function(){
+            $('.nav-comp').fadeIn('fast');
+        });
+    }
 });
